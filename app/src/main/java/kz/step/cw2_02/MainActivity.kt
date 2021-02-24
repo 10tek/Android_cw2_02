92b4cb48138ef5c84badb6e820034691f999b0ce
10tekinvo@gmail.com <10tekinvo@gmail.com>
2021-02-24 21:53:00 +0600

    Добавил Views и инициализацию в MainActivity

--- app/src/main/java/kz/step/cw2_02/MainActivity.kt
+++ app/src/main/java/kz/step/cw2_02/MainActivity.kt
@@ -2,10 +2,34 @@ package kz.step.cw2_02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var registrationButton: Button? = null
+    var nameEditText: EditText? = null
+    var emailEditText: EditText? = null
+    var passwordEditText: EditText? = null
+    var passwordConfirmEditText: EditText? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeViews()
+        initializeLiseners()
+    }
+
+    fun initializeViews(){
+        registrationButton = findViewById(R.id.button_main_activity_registration_button)
+        nameEditText = findViewById(R.id.textview_main_activity_name_text)
+        emailEditText = findViewById(R.id.textview_main_activity_email_text)
+        passwordEditText = findViewById(R.id.textview_main_activity_password_text)
+        passwordConfirmEditText = findViewById(R.id.textview_main_activity_password_confirm_text)
+    }
+
+    fun initializeLiseners(){
+
     }
 }
